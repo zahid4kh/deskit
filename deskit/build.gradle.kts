8b9fd33049a32158b2029a5161b5360dda5a4555
zahid4kh <halilzahid@gmail.com>
2025-05-14 21:25:48 +0200

    confirmation dialog

--- deskit/build.gradle.kts
+++ deskit/build.gradle.kts
@@ -19,3 +19,14 @@ dependencies {
     implementation(compose.material3)
     implementation(compose.materialIconsExtended)
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
