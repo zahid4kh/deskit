343fde4282b420edf18557671a940e6147323155
zahid4kh <halilzahid@gmail.com>
2025-06-29 14:37:32 +0200

    FolderChooserDialog() -> removed duplicate getFileIcon fuction; made the sample function public(forgot this in past release)

--- src/main/kotlin/deskit/dialogs/file/folderchooser/FolderChooserDialog.kt
+++ src/main/kotlin/deskit/dialogs/file/folderchooser/FolderChooserDialog.kt
@@ -28,7 +28,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpSize
@@ -38,6 +37,7 @@ import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberDialogState
 import deskit.dialogs.info.InfoDialog
 import deskit.resources.*
+import deskit.utils.getFileIcon
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -330,71 +330,6 @@ private fun PathSegmentsSection(
     }
 }
 
-@Composable
-private fun getFileIcon(file: File): Painter {
-    if (file.isDirectory) return painterResource(Res.drawable.folder)
-
-    val extension = file.extension.lowercase()
-    return when (extension) {
-        // Images
-        "webp" -> painterResource(Res.drawable.image)
-        "png" -> painterResource(Res.drawable.png)
-        "gif" -> painterResource(Res.drawable.gif)
-        "bmp" -> painterResource(Res.drawable.bmp)
-        "svg" -> painterResource(Res.drawable.svg)
-        "jpg", "jpeg" -> painterResource(Res.drawable.jpg)
-
-        // Docs
-        "pdf" -> painterResource(Res.drawable.pdf)
-        "doc", "docx" -> painterResource(Res.drawable.doc)
-        "xls", "xlsx" -> painterResource(Res.drawable.xls)
-        "ppt", "pptx" -> painterResource(Res.drawable.ppt)
-        "txt" -> painterResource(Res.drawable.txt)
-        "md" -> painterResource(Res.drawable.markdown)
-
-        // Code
-        "rb", "go", "rs" -> painterResource(Res.drawable.code)
-        "js" -> painterResource(Res.drawable.javascript)
-        "cs" -> painterResource(Res.drawable.csharp)
-        "php" -> painterResource(Res.drawable.php)
-        "ts" -> painterResource(Res.drawable.typescript)
-        "c" -> painterResource(Res.drawable.c)
-        "cpp" -> painterResource(Res.drawable.cplusplus)
-        "java" -> painterResource(Res.drawable.java)
-        "py" -> painterResource(Res.drawable.python)
-        "kt" -> painterResource(Res.drawable.kotlin)
-        "html", "htm" -> painterResource(Res.drawable.html5)
-        "htmx" -> painterResource(Res.drawable.htmx)
-        "css" -> painterResource(Res.drawable.css)
-        "xml" -> painterResource(Res.drawable.xml)
-        "yml", "yaml" -> painterResource(Res.drawable.yaml)
-        "json" -> painterResource(Res.drawable.json)
-        "db" -> painterResource(Res.drawable.sql)
-
-        // Archive
-        "rar", "7z", "tar", "gz" -> painterResource(Res.drawable.archive)
-        "zip" -> painterResource(Res.drawable.zip)
-
-        // Audio
-        "mp3", "wav", "flac", "aac", "ogg" -> painterResource(Res.drawable.audio)
-
-        // Video
-        "mp4", "avi", "mkv", "mov", "wmv", "flv" -> painterResource(Res.drawable.video)
-
-        // Fonts
-        "ttf", "otf", "woff", "woff2" -> painterResource(Res.drawable.font)
-
-        // Executable
-        "app" -> painterResource(Res.drawable.exe)
-        "exe" -> painterResource(Res.drawable.exe)
-        "msi" -> painterResource(Res.drawable.msi)
-        "rpm" -> painterResource(Res.drawable.rpm)
-        "dmg" -> painterResource(Res.drawable.dmg)
-        "deb" -> painterResource(Res.drawable.debian)
-
-        else -> painterResource(Res.drawable.document)
-    }
-}
 
 /**
  * A sample composable function demonstrating the usage of the [FolderChooserDialog].
@@ -414,7 +349,7 @@ private fun getFileIcon(file: File): Painter {
  * @sample deskit.dialogs.file.folderchooser.FolderChooserDialogSample
  */
 @Composable
-private fun FolderChooserDialogSample(){
+fun FolderChooserDialogSample(){
     var showFolderChooserDialog by remember { mutableStateOf(false) }
     var text by remember { mutableStateOf("") }
 
