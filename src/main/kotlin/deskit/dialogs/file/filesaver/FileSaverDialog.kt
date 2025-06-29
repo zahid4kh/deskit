231f068f818d647e927472c013923a0d21e5ac11
zahid4kh <halilzahid@gmail.com>
2025-06-29 14:34:46 +0200

    FileSaverDialog() -> - added fileIconColor, folderIconColor, resizableFileInfoDialog to the constructor - Implemented FileInfoDialog with file metadata (name, type, size, location, modified date) - Added per-item hover states with animated info icon buttons for files and folders - Added copy-to-clipboard functionality for file paths with visual feedback - Calculating actual folder size recursively - Displaying folder file count and total size for directories - Added tooltips and success messages for clipboard operations

--- src/main/kotlin/deskit/dialogs/file/filesaver/FileSaverDialog.kt
+++ src/main/kotlin/deskit/dialogs/file/filesaver/FileSaverDialog.kt
@@ -13,37 +13,23 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 package deskit.dialogs.file.filesaver
 
-
-import androidx.compose.animation.*
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowCircleLeft
-import androidx.compose.material.icons.filled.CreateNewFolder
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogWindow
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberDialogState
 import deskit.dialogs.info.InfoDialog
-import deskit.resources.*
-import kotlinx.coroutines.CoroutineScope
+import deskit.utils.FileInfoDialog
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.resources.painterResource
 import java.awt.Dimension
 import java.io.File
 
@@ -73,7 +59,10 @@ import java.io.File
 fun FileSaverDialog(
     title: String = "Save As",
     suggestedFileName: String = "",
+    fileIconColor: Color = MaterialTheme.colorScheme.primary,
+    folderIconColor: Color = MaterialTheme.colorScheme.tertiary,
     extension: String,
+    resizableFileInfoDialog: Boolean = true,
     onSave: (File) -> Unit,
     onCancel: () -> Unit
 ) {
@@ -82,6 +71,7 @@ fun FileSaverDialog(
     var showFileExistsDialog by remember { mutableStateOf(false) }
     var currentDir by remember { mutableStateOf(File("$homeDir/Downloads")) }
     val coroutineScope = rememberCoroutineScope()
+    var selectedFileForInfo by remember { mutableStateOf<File?>(null) }
 
     val pathSegments = generateSequence(currentDir) { it.parentFile }
         .toList()
@@ -102,6 +92,14 @@ fun FileSaverDialog(
 
     LaunchedEffect(pathSegments) {
         pathScrollState.animateScrollTo(pathScrollState.maxValue)
+    }
+
+    selectedFileForInfo?.let { file ->
+        FileInfoDialog(
+            file = file,
+            onClose = { selectedFileForInfo = null },
+            resizable = resizableFileInfoDialog
+        )
     }
 
     DialogWindow(
@@ -180,6 +178,11 @@ fun FileSaverDialog(
                             pathScrollState.animateScrollTo(pathScrollState.maxValue)
                         }
                     },
+                    onShowFileInfo = {file ->
+                        selectedFileForInfo = file
+                    },
+                    folderIconColor = folderIconColor,
+                    fileIconColor = fileIconColor,
                     modifier = Modifier.weight(1f)
                 )
 
@@ -228,320 +231,6 @@ fun FileSaverDialog(
             message = "A file named \"$displayFileName\" already exists in this folder. Please choose a different name.",
             onClose = { showFileExistsDialog = false }
         )
-    }
-}
-
-
-@Composable
-private fun NavigationButtonsSection(
-    coroutineScope: CoroutineScope,
-    pathScrollState: ScrollState,
-    currentDir: File,
-    onBackClicked: (File) -> Unit,
-    onNewFolderClicked: () -> Unit
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            if (currentDir.parentFile != null) {
-                IconButton(
-                    onClick = {
-                        currentDir.parentFile?.let { parent ->
-                            onBackClicked(parent)
-                            coroutineScope.launch {
-                                pathScrollState.animateScrollTo(pathScrollState.maxValue)
-                            }
-                        }
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowCircleLeft,
-                        contentDescription = "Back"
-                    )
-                }
-            }
-            Spacer(Modifier.width(3.dp))
-            Text("Choose Folder", style = MaterialTheme.typography.labelLarge)
-        }
-
-        Box(
-            contentAlignment = Alignment.Center,
-        ) {
-            IconButton(onClick = onNewFolderClicked) {
-                Icon(Icons.Default.CreateNewFolder, contentDescription = "New Folder")
-            }
-        }
-    }
-}
-
-
-@Composable
-private fun PathSegmentsSection(
-    pathScrollState: ScrollState,
-    pathSegments: List<File>,
-    onFolderSelected: (File) -> Unit
-) {
-    Box(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            modifier = Modifier
-                .horizontalScroll(pathScrollState)
-                .padding(bottom = 8.dp, end = 12.dp)
-        ) {
-            pathSegments.forEachIndexed { index, dir ->
-                Text(
-                    text = dir.name.ifBlank { "." },
-                    color = if (index == pathSegments.lastIndex)
-                        MaterialTheme.colorScheme.primary else LocalContentColor.current,
-                    modifier = Modifier
-                        .clip(MaterialTheme.shapes.medium)
-                        .clickable { onFolderSelected(dir) }
-                        .padding(8.dp),
-                    style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Bold)
-                )
-                if (index != pathSegments.lastIndex) {
-                    Text("/", style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Bold))
-                }
-            }
-        }
-        HorizontalScrollbar(
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .fillMaxWidth()
-                .padding(end = 12.dp),
-            adapter = rememberScrollbarAdapter(pathScrollState),
-            style = LocalScrollbarStyle.current.copy(
-                hoverColor = MaterialTheme.colorScheme.outline,
-                unhoverColor = MaterialTheme.colorScheme.primaryContainer
-            )
-        )
-    }
-}
-
-
-@Composable
-private fun FileNameInputSection(
-    fileName: String,
-    extension: String,
-    onFileNameChanged: (String) -> Unit
-) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        OutlinedTextField(
-            value = fileName,
-            onValueChange = onFileNameChanged,
-            label = { Text("File name", style = MaterialTheme.typography.bodyMedium) },
-            modifier = Modifier.weight(1f),
-            shape = MaterialTheme.shapes.large,
-            placeholder = { Text("Enter filename${extension}",
-                style = MaterialTheme.typography.bodyMedium) },
-            supportingText = {
-                Text(
-                    "Extension $extension will be added automatically",
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
-        )
-
-        Spacer(Modifier.width(8.dp))
-
-        Text(
-            text = extension,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.primary
-        )
-    }
-}
-
-
-@Composable
-private fun FilesAndFoldersListSection(
-    items: List<File>,
-    onFolderClicked: (File) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Box(modifier = modifier) {
-        val listState = rememberLazyListState()
-
-        Box(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(
-                state = listState,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(end = 12.dp)
-            ) {
-                items(items) { item ->
-                    if (item.isDirectory) {
-                        Row(
-                            Modifier
-                                .fillMaxWidth()
-                                .clip(MaterialTheme.shapes.medium)
-                                .clickable { onFolderClicked(item) }
-                                .padding(9.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                painter = painterResource(Res.drawable.folder),
-                                contentDescription = null,
-                                tint = MaterialTheme.colorScheme.tertiary,
-                                modifier = Modifier.size(22.dp)
-                            )
-                            Spacer(Modifier.width(8.dp))
-                            Text(
-                                text = item.name,
-                                overflow = TextOverflow.Ellipsis,
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                    } else {
-                        Row(
-                            Modifier
-                                .fillMaxWidth()
-                                .padding(9.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                painter = getFileIcon(item),
-                                contentDescription = null,
-                                tint = MaterialTheme.colorScheme.outline.copy(alpha = 0.6f),
-                                modifier = Modifier.size(22.dp)
-                            )
-                            Spacer(Modifier.width(8.dp))
-                            Text(
-                                text = item.name,
-                                overflow = TextOverflow.Ellipsis,
-                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
-                                style = MaterialTheme.typography.bodyMedium
-                            )
-                        }
-                    }
-                }
-            }
-
-            VerticalScrollbar(
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .fillMaxHeight(),
-                adapter = rememberScrollbarAdapter(listState),
-                style = LocalScrollbarStyle.current.copy(
-                    hoverColor = MaterialTheme.colorScheme.outline,
-                    unhoverColor = MaterialTheme.colorScheme.primaryContainer
-                )
-            )
-        }
-    }
-}
-
-@Composable
-private fun NewFolderCreationSection(
-    visible: Boolean,
-    folderName: String,
-    onFolderNameChanged: (String) -> Unit,
-    onCancel: () -> Unit,
-    onCreateFolder: () -> Unit
-) {
-    AnimatedVisibility(
-        visible = visible,
-        enter = slideInHorizontally() + fadeIn(),
-        exit = slideOutHorizontally(targetOffsetX = { -it }) + fadeOut()
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(
-                value = folderName,
-                onValueChange = onFolderNameChanged,
-                label = { Text("New Folder Name", style = MaterialTheme.typography.bodyMedium) },
-                modifier = Modifier.weight(1f),
-                shape = MaterialTheme.shapes.large
-            )
-            Spacer(Modifier.width(3.dp))
-            TextButton(
-                onClick = onCancel
-            ) {
-                Text("Cancel", color = MaterialTheme.colorScheme.error, style = MaterialTheme.typography.bodyMedium)
-            }
-            Spacer(Modifier.width(3.dp))
-            OutlinedButton(
-                onClick = onCreateFolder,
-                enabled = folderName.isNotBlank(),
-                shape = MaterialTheme.shapes.medium
-            ) {
-                Text(text = "Create", style = MaterialTheme.typography.bodyMedium)
-            }
-        }
-    }
-}
-
-
-@Composable
-private fun getFileIcon(file: File): Painter {
-    if (file.isDirectory) return painterResource(Res.drawable.folder)
-
-    val extension = file.extension.lowercase()
-    return when (extension) {
-        // Images
-        "webp" -> painterResource(Res.drawable.image)
-        "png" -> painterResource(Res.drawable.png)
-        "gif" -> painterResource(Res.drawable.gif)
-        "bmp" -> painterResource(Res.drawable.bmp)
-        "svg" -> painterResource(Res.drawable.svg)
-        "jpg", "jpeg" -> painterResource(Res.drawable.jpg)
-
-        // Docs
-        "pdf" -> painterResource(Res.drawable.pdf)
-        "doc", "docx" -> painterResource(Res.drawable.doc)
-        "xls", "xlsx" -> painterResource(Res.drawable.xls)
-        "ppt", "pptx" -> painterResource(Res.drawable.ppt)
-        "txt" -> painterResource(Res.drawable.txt)
-        "md" -> painterResource(Res.drawable.markdown)
-
-        // Code
-        "rb", "go", "rs" -> painterResource(Res.drawable.code)
-        "js" -> painterResource(Res.drawable.javascript)
-        "cs" -> painterResource(Res.drawable.csharp)
-        "php" -> painterResource(Res.drawable.php)
-        "ts" -> painterResource(Res.drawable.typescript)
-        "c" -> painterResource(Res.drawable.c)
-        "cpp" -> painterResource(Res.drawable.cplusplus)
-        "java" -> painterResource(Res.drawable.java)
-        "py" -> painterResource(Res.drawable.python)
-        "kt" -> painterResource(Res.drawable.kotlin)
-        "html", "htm" -> painterResource(Res.drawable.html5)
-        "htmx" -> painterResource(Res.drawable.htmx)
-        "css" -> painterResource(Res.drawable.css)
-        "xml" -> painterResource(Res.drawable.xml)
-        "yml", "yaml" -> painterResource(Res.drawable.yaml)
-        "json" -> painterResource(Res.drawable.json)
-        "db" -> painterResource(Res.drawable.sql)
-
-        // Archive
-        "rar", "7z", "tar", "gz" -> painterResource(Res.drawable.archive)
-        "zip" -> painterResource(Res.drawable.zip)
-
-        // Audio
-        "mp3", "wav", "flac", "aac", "ogg" -> painterResource(Res.drawable.audio)
-
-        // Video
-        "mp4", "avi", "mkv", "mov", "wmv", "flv" -> painterResource(Res.drawable.video)
-
-        // Fonts
-        "ttf", "otf", "woff", "woff2" -> painterResource(Res.drawable.font)
-
-        // Executable
-        "app" -> painterResource(Res.drawable.exe)
-        "exe" -> painterResource(Res.drawable.exe)
-        "msi" -> painterResource(Res.drawable.msi)
-        "rpm" -> painterResource(Res.drawable.rpm)
-        "dmg" -> painterResource(Res.drawable.dmg)
-        "deb" -> painterResource(Res.drawable.debian)
-
-        else -> painterResource(Res.drawable.document)
     }
 }
 
