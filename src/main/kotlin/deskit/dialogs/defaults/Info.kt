b74108f4c186405cff8c3f3efd0f8407681fb362
zahid4kh <halilzahid@gmail.com>
2025-07-08 17:26:21 +0200

    added kdoc comments to defaults/Info.kt

--- src/main/kotlin/deskit/dialogs/defaults/Info.kt
+++ src/main/kotlin/deskit/dialogs/defaults/Info.kt
@@ -4,13 +4,27 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Represents the color scheme for InfoDialog components.
+ *
+ * @param iconTint Color applied to the dialog icon
+ * @param okButtonColor Container color for the OK button
+ * @param okButtonTextColor Text color for the OK button
+ */
 data class InfoDialogColors(
     val iconTint: Color,
     val okButtonColor: Color,
     val okButtonTextColor: Color
 )
 
+/**
+ * Contains default values for InfoDialog colors.
+ */
 object InfoDialogDefaults{
+    /**
+     * Creates a [InfoDialogColors] instance with the provided color values.
+     * Any unspecified colors will use the default Material3 theme colors.
+     */
     @Composable
     fun colors(
         iconTint: Color = MaterialTheme.colorScheme.primary,
