7a8f35a08be30ce986067eeefb36f8515b58e8ab
zahid4kh <halilzahid@gmail.com>
2025-07-08 17:28:48 +0200

    added kdoc comments to defaults/FileSaver.kt

--- src/main/kotlin/deskit/dialogs/defaults/FileSaver.kt
+++ src/main/kotlin/deskit/dialogs/defaults/FileSaver.kt
@@ -4,6 +4,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+/**
+ * Represents the color scheme for FileSaverDialog components.
+ *
+ * @param folderIconColor Color applied to folder icons
+ * @param fileIconColor Color applied to file icons
+ * @param fileAndFolderListBG Background color of the file/folder list area
+ * @param folderNameColor Text color for folder names
+ * @param fileNameColor Text color for file names (dimmed since not selectable)
+ * @param infoIconTint Color for info icons that appear on hover
+ * @param scrollbarHoverColor Scrollbar color when hovered
+ * @param scrollbarUnhoverColor Scrollbar color when not hovered
+ */
 data class FileSaverColors(
     val folderIconColor: Color,
     val fileIconColor: Color,
@@ -16,8 +28,14 @@ data class FileSaverColors(
     val tooltipColor: Color
 )
 
-
+/**
+ * Contains default values for FileSaverDialog colors.
+ */
 object FileSaverDefaults{
+    /**
+     * Creates a [FileSaverColors] instance with the provided color values.
+     * Any unspecified colors will use the default Material3 theme colors.
+     */
     @Composable
     fun colors(
         folderIconColor: Color = MaterialTheme.colorScheme.primary,
